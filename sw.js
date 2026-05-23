563156b35c2ce4656ee22033ff87a8073b57d0f0
yerrakamalkumar1 <yerrakamalkumar1@gmail.com>
2026-05-23 17:00:29 +0530

    Make React mobile UI the live app

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "connecthub-v9";
+const CACHE_NAME = "connecthub-v10";
 const APP_SHELL = [
   "index.html",
   "index.css",
@@ -15,6 +15,7 @@ const APP_SHELL = [
 ];
 
 self.addEventListener("install", event => {
+  self.skipWaiting();
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
 });
 
@@ -22,7 +23,7 @@ self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(keys =>
       Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
-    )
+    ).then(() => self.clients.claim())
   );
 });
 
